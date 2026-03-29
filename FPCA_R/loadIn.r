fdf6bd5e1fe95a64cd8ed80feb5a3924ab526657
Henry Heyden <henryden22@gmail.com>
2026-03-28 21:11:19 -0500

    add loadIn script!
    This was used before I even had a github repo. Really important file for replicability!

---
+++ FPCA_R/loadIn.r
@@ -0,0 +1,57 @@
+library(tidyverse)
+data("acoustics", package = "voicequality")
+data("egg", package = "voicequality")
+
+gujarati <- acoustics %>% 
+  filter(language == "Gujarati") %>% 
+  left_join(egg) %>% 
+  filter(timepoint == 5)
+
+hmong <- acoustics %>% 
+  filter(language == "Hmong") %>% 
+  left_join(egg) %>% 
+  filter(timepoint == 5)
+
+mandarin <- acoustics %>% 
+  filter(language == "Mandarin") %>% 
+  left_join(egg) %>% 
+  filter(timepoint == 5)
+
+luchun <- acoustics %>% 
+  filter(language == "Luchun") %>% 
+  left_join(egg) %>% 
+  filter(timepoint == 5)
+
+yi <- acoustics %>% 
+  filter(language == "Yi") %>% 
+  left_join(egg) %>% 
+  filter(timepoint == 5)
+
+bo <- acoustics %>% 
+  filter(language == "Bo") %>% 
+  left_join(egg) %>% 
+  filter(timepoint == 5)
+
+miao <- acoustics %>% 
+  filter(language == "Miao") %>% 
+  left_join(egg) %>% 
+  filter(timepoint == 5)
+
+zapotec <- acoustics %>% 
+  filter(language == "Zapotec") %>% 
+  left_join(egg) %>% 
+  filter(timepoint == 5)
+
+all_data <- rbind(gujarati, hmong, mandarin, luchun, yi, bo, miao, zapotec) %>% 
+  filter(!is.na(CQ)) %>% 
+  filter(!is.na(strF0)) %>% 
+  # filter(SQ2_SQ1 > 0) %>% 
+  # filter(SQ2_SQ1 < 1) %>% 
+  # filter(SQ4_SQ3 > 0) %>% 
+  # filter(SQ4_SQ3 < 1) %>% 
+  mutate(LPhon = paste(language, phonation, sep = "-") %>% as.factor()) %>% 
+  mutate(SQ = SQ2_SQ1 / SQ4_SQ3) %>% 
+  filter(SQ > 0) %>% 
+  filter(SQ < 1) %>% 
+  filter(CQ_H > 0)
+
